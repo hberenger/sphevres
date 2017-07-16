63b7f4ca2a1e1b63a2e95744c6d491e682b9c712
Hervé Bérenger <hberenger@Mac-mini-de-Herve-4.local>
2017-07-16 17:45:49 +0200

    Extract GVRMain and load biosphere model

---
+++ app/src/main/java/com/nocomment/sphevres/Main.java
@@ -0,0 +1,30 @@
+package com.nocomment.sphevres;
+
+import org.gearvrf.GVRContext;
+import org.gearvrf.GVRMain;
+import org.gearvrf.GVRScene;
+import org.gearvrf.scene_objects.GVRModelSceneObject;
+
+final class Main extends GVRMain {
+    @Override
+    public void onInit(GVRContext gvrContext) throws Throwable {
+        GVRScene scene = gvrContext.getMainScene();
+
+        try {
+            GVRModelSceneObject geosphere = gvrContext.getAssetLoader().loadModel("biosphere3-3ds/biosphere3.3ds");
+            geosphere.getTransform().setPosition(0.f, 0.f, 0.f);
+            scene.addSceneObject(geosphere);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public SplashMode getSplashMode() {
+        return SplashMode.NONE;
+    }
+
+    @Override
+    public void onStep() {
+    }
+}
--- app/src/main/java/com/nocomment/sphevres/MainActivity.java
+++ app/src/main/java/com/nocomment/sphevres/MainActivity.java
@@ -3,11 +3,6 @@ package com.nocomment.sphevres;
 import android.os.Bundle;
 
 import org.gearvrf.GVRActivity;
-import org.gearvrf.GVRAndroidResource;
-import org.gearvrf.GVRContext;
-import org.gearvrf.GVRMain;
-import org.gearvrf.GVRSceneObject;
-import org.gearvrf.GVRTexture;
 
 public class MainActivity extends GVRActivity {
 
@@ -17,24 +12,4 @@ public class MainActivity extends GVRActivity {
         setMain(new Main(), "gvr.xml");
     }
 
-    private final class Main extends GVRMain {
-        @Override
-        public void onInit(GVRContext gvrContext) throws Throwable {
-            GVRTexture texture = gvrContext.getAssetLoader().loadTexture(new GVRAndroidResource(gvrContext, R.drawable.__default_splash_screen__));
-
-            GVRSceneObject quad = new GVRSceneObject(gvrContext, 4, 2, texture);
-            quad.getTransform().setPosition(0, 0, -3);
-
-            gvrContext.getMainScene().addSceneObject(quad);
-        }
-
-        @Override
-        public SplashMode getSplashMode() {
-            return SplashMode.NONE;
-        }
-
-        @Override
-        public void onStep() {
-        }
-    }
 }
