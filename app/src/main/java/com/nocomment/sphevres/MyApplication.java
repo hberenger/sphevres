125ba04ac03a12cf27ec150c1572a141ab3d9652
Hervé Bérenger <hberenger@mac-mini-de-herve-4.home>
2017-07-31 03:22:27 +0200

    Prevent service from being started twice (it shouldn’t be started by its own process application class)

--- app/src/main/java/com/nocomment/sphevres/MyApplication.java
+++ app/src/main/java/com/nocomment/sphevres/MyApplication.java
@@ -1,9 +1,12 @@
 package com.nocomment.sphevres;
 
+import android.app.ActivityManager;
 import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+
+import java.util.List;
 
 
 public class MyApplication extends Application {
@@ -15,11 +18,14 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         instance = this;
+        Log.i(TAG, "App class created !");
 
-        Intent service = new Intent(this.getApplicationContext(), AngelService.class);
-        service.setAction(AngelService.ACTION_START);
-        Log.i(TAG, "App starts service !");
-        startService(service);
+        if (!isAngelProcess()) {
+            Intent service = new Intent(this.getApplicationContext(), AngelService.class);
+            service.setAction(AngelService.ACTION_START);
+            Log.i(TAG, "App starts service !");
+            startService(service);
+        }
     }
     @Override
     public Context getApplicationContext() {
@@ -27,6 +33,26 @@ public class MyApplication extends Application {
     }
     public static MyApplication getInstance() {
         return instance;
+    }
+
+    private String processName() {
+        int pid = android.os.Process.myPid();
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> infos = manager.getRunningAppProcesses();
+        if (infos != null) {
+            for (ActivityManager.RunningAppProcessInfo processInfo : infos) {
+                if (processInfo.pid == pid) {
+                    return processInfo.processName;
+                }
+            }
+        }
+        return "?";
+    }
+
+    private boolean isAngelProcess() {
+        String processName = processName();
+        String angelProcessName = getString(R.string.angel_process_name);
+        return processName.contains(angelProcessName);
     }
 
 }
